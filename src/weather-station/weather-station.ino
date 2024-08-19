e33ee320dde433a71b3fdfed1fe4cc152a8eada3
Horbee <n.horox@gmail.com>
2024-08-19 21:28:42 +0200

    Wake up screen by pressing button

--- src/weather-station/weather-station.ino
+++ src/weather-station/weather-station.ino
@@ -1,6 +1,7 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <LowPower.h>
 
 #include "DisplayUtils.h"
 
@@ -8,6 +9,10 @@
 #define SCREEN_HEIGHT 32 
 
 #define BME280_ADDRESS 0x76 
+
+#define BTN_PIN 2
+
+volatile bool isScreenOn = false, btnPressed = false;
 
 Adafruit_BME280 bme; 
 DisplayUtils displayUtils(SCREEN_WIDTH, SCREEN_HEIGHT); 
@@ -22,11 +27,28 @@ void setup() {
     while (1) delay(10);
   }
   displayUtils.initDisplay();
+
+  pinMode(BTN_PIN, INPUT);
+  attachInterrupt(0, callback, CHANGE);
 }
 
 void loop() {
-  printValues();
-  delay(1000);
+  int btnState = digitalRead(BTN_PIN);
+  if (btnState == 0) btnPressed = false;
+
+  if (isScreenOn) {
+    Serial.println("Screen is on...");
+    printValues();
+    delay(1000);
+  } else {
+    Serial.println("Going to sleep...");
+    displayUtils.clearDisplay();
+    displayUtils.updateDisplay();
+    // Debounce delay
+    delay(50);
+    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
+  }
+
 }
 
 void printValues() {
@@ -50,3 +72,12 @@ void printValues() {
   Serial.println("T: " + temp + " °C, P: " + pressure + " hPa, Hum " + humidity + " %");
 }
 
+void callback() {
+  if (btnPressed) return;
+
+  btnPressed = true;
+  Serial.println("Callback called");
+  isScreenOn = !isScreenOn;
+  Serial.print("isScreenOn: ");
+  Serial.println(isScreenOn);
+}
